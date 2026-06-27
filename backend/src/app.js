68148412ab61c0cd334d1afe45f5b64e0959f136
samuel-proxy <samuelmusyoka473@gmail.com>
2026-06-27 14:43:27 +0300

    Project foundation with React and Express setup

---
+++ backend/src/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/api/health", (req, res) => {
+    res.json({
+        success: true,
+        message: "Tavani API is running"
+    });
+});
+
+module.exports  = app;
